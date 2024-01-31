6e60bb970aa0ec19727933e98ffa15bd5a584523
AM-SagarThakare <sagar.thakare@angularminds.in>
2024-02-01 01:22:06 +0530

    backend starter kit added

---
+++ server/src/validations/auth.validation.js
@@ -0,0 +1,59 @@
+const Joi = require("joi");
+const { password } = require("./custom.validation");
+
+const register = {
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().custom(password),
+    name: Joi.string().required(),
+  }),
+};
+
+const login = {
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+};
+
+const socialLogin = {
+  body: Joi.object().keys({
+    token: Joi.string().required(),
+  }),
+};
+
+const forgotPassword = {
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+  }),
+};
+
+const resetPassword = {
+  query: Joi.object().keys({
+    token: Joi.string().required(),
+  }),
+  body: Joi.object().keys({
+    password: Joi.string().required().custom(password),
+  }),
+};
+
+const verifyEmail = {
+  query: Joi.object().keys({
+    token: Joi.string().required(),
+  }),
+};
+
+const verifyOTP = {
+  body : Joi.object().keys({
+    otp : Joi.number().required() 
+  })
+}
+module.exports = {
+  register,
+  login,
+  socialLogin,
+  forgotPassword,
+  resetPassword,
+  verifyEmail,
+  verifyOTP
+};
