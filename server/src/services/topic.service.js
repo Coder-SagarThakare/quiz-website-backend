5f88c8ac0e421114b0e20bc98c2be5e9f504ee49
Sagar-Thakare <saggythakare01@gmail.com>
2024-02-05 15:25:56 +0530

    add topic api done

--- server/src/services/topic.service.js
+++ server/src/services/topic.service.js
@@ -1,13 +1,17 @@
 const httpStatus = require("http-status");
-const { Topic } = require("../models");
+const { Topic, Subject } = require("../models");
 const ApiError = require("../utils/ApiError");
 
 const addNewTopic = async (subjectId, topic) => {
+  const isValidSubjectId = await Subject.findOne({ _id: subjectId });
+
+  if (!isValidSubjectId)
+    throw new ApiError(httpStatus.NOT_FOUND, "Enter valid mongo id");
+
   const isAlreadyAdded = await Topic.findOne({
     subject: subjectId,
     name: topic,
   });
-
 
   if (isAlreadyAdded)
     throw new ApiError(httpStatus.CONFLICT, `${topic} topic is already added`);
